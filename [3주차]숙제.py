4c21b7801971994953c9ca1a341834cceb539345
zihyun321 <65486257+zihyun321@users.noreply.github.com>
2020-05-24 18:40:24 +0900

    Add files via upload

---
+++ [3주차]숙제.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+genieChart = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+count = 0
+for genie in genieChart:
+    count+= 1
+    title = genie.select_one('td.info > a.title')
+    singer = genie.select_one('td.info > a.artist.ellipsis')
+    print(count, title.text.strip(), singer.text)
+   
